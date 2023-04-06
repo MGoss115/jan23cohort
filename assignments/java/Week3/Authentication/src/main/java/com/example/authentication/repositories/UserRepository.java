4ecb971802da7b072114f296d165e8cf4f753bf7
mligon115 <mligon115@yahoo.com>
2023-04-06 17:15:17 -0500

    saving

--- assignments/java/Week3/Authentication/src/main/java/com/example/authentication/repositories/UserRepository.java
+++ assignments/java/Week3/Authentication/src/main/java/com/example/authentication/repositories/UserRepository.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.authentication.models.User;
-
 @Repository
 public interface UserRepository extends CrudRepository<User, Long>{
 	
